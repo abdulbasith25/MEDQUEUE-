69153a8373bb0a18e3c7d2695a75e67e1e4e90dc
abdul basith <abdulbasith2590@gmail.com>
2026-05-03 12:08:23 +0530

    changedd from core java thread pool to spring managed thread pool

--- src/main/java/com/appointment/config/AsyncConfig.java
+++ src/main/java/com/appointment/config/AsyncConfig.java
@@ -3,21 +3,33 @@ package com.appointment.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @Configuration
-@EnableAsync  // ← Activates @Async processing across the whole app
+@EnableAsync  
 public class AsyncConfig {
 
     @Bean(name = "notificationExecutor")
     public Executor notificationExecutor() {
-        return Executors.newFixedThreadPool(3); // 3 is enough for email sending
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("Notification-");
+        executor.initialize();
+        return executor;
     }
 
-    @Bean(name = "pushNotificationExecuter")
-    public Executor pushNotificationExecuter(){
-        return Executors.newFixedThreadPool(2);
+    @Bean(name = "pushNotificationExecutor")
+    public Executor pushNotificationExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(19);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("PushNotification-");
+        executor.initialize();
+        return executor;
     }
 }
